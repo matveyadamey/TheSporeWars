f585fef14911044fdf81994c6fde8e05822886c4
RazWars <156277907+RazWars@users.noreply.github.com>
2025-01-11 10:36:19 +0300

    0.0.4

--- Assets/Scripts/Menu/Manage.cs
+++ Assets/Scripts/Menu/Manage.cs
@@ -1,10 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Manage : MonoBehaviour
 {
+    [DllImport("__Internal")]
+    private static extern void GameReadyAPI();
+
+    [SerializeField] bool isYandex;
+
+    [SerializeField] GameObject ExitButton;
+    [SerializeField] GameObject Yandex;
+    [SerializeField] GameObject AntiAliasing;
+    [SerializeField] GameObject PostProcessing;
+
+    private void Start()
+    {
+        if (isYandex)
+        {
+            ExitButton.SetActive(false);
+            Yandex.SetActive(true);
+            AntiAliasing.SetActive(false);
+            PostProcessing.SetActive(false);
+            GameReadyAPI();
+        }
+    }
+
     public void LoadScene(int sceneid)
     {
         SceneManager.LoadScene(sceneid);
@@ -13,5 +36,10 @@ public class Manage : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void Telegram()
+    {
+        Application.OpenURL("https://t.me/Yuhgames");
     }
 }
